f207ed61d334013d00d55f8400ae6751bdca8032
Sheeba Govindan <sheegovi@in.ibm.com>
2020-04-22 15:03:12 +0530

    first commit

---
+++ src/main/java/com/ibm/FoodNutritionApp/FoodNutritionAppApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.FoodNutritionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodNutritionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodNutritionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/FoodNutritionApp/FoodNutritionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.FoodNutritionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodNutritionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
